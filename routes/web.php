05b278b32603beae0ea66631fa1bb4a3913bb579
ShawnCampo <shawnmikel498@gmail.com>
2025-04-23 16:10:35 +0800

    asdasd

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
+//about us
+Route::get('/about-us', function () {
+    return view('aboutus');
+});
+
+//meet the team
+Route::get('/meet-the-team', function () {
+    return view('meettheteam');
+})->name('meet-the-team');
+
+//partner with us
+Route::get('/partner-with-us', function () {
+    return view('partnerwithus');
+})->name('partner-with-us');
+
+//contact us
+Route::get('/contact-us', function () {
+    return view('contactus');
+})->name('contact-us');
+
